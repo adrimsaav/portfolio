21af8be06036ca3c875bad76c647947cfd9dc809
adrimsaav <adrimsaav@gmail.com>
2023-12-29 23:11:23 -0500

    fix styling on navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,13 +5,10 @@ export default function NavBar() {
     return (
         <nav className="navbar">
             {/* <img src="" alt="" /> */}
-            <Link to="/">Home</Link>
-            &nbsp; | &nbsp;
-            <Link to="/about">About</Link>
-            &nbsp; | &nbsp;
-            <Link to="/projects">Projects</Link>
-            &nbsp; | &nbsp;
-            <Link to="/contact">Contact</Link>
+            <Link to="/" className='navbar-item'>Home</Link>
+            <Link to="/about" className='navbar-item'>About</Link>
+            <Link to="/projects" className='navbar-item'>Projects</Link>
+            <Link to="/contact" className='navbar-item'>Contact</Link>
         </nav>
     );
 }
