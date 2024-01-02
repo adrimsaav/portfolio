e52e3c4aa46ac11f5de59f4e4a7ceeb9c8f59dae
adrimsaav <adrimsaav@gmail.com>
2024-01-02 14:04:05 -0500

    stylize welcome/intro card

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,8 +4,9 @@ import './NavBar.css';
 export default function NavBar() {
     return (
         <nav className="navbar">
-            {/* <img src="" alt="" /> */}
-            <Link to="/" className='navbar-item'>Home</Link>
+            <Link to="/" className='home-icon'>
+                <img src="/adriana.png" className='home-icon'/>
+            </Link>
             <Link to="about" className='navbar-item'>About</Link>
             <Link to="projects" className='navbar-item'>Projects</Link>
             <Link to="contact" className='navbar-item'>Contact</Link>
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -7,15 +7,18 @@ import github from '../../assets/github.png';
 import linkedin from '../../assets/linkedin.png';
 import gmail from '../../assets/gmail.png';
 import cv from '../../assets/cv.png';
+import name from '../../assets/adriana-fullname.png';
 import './Welcome.css'
 
 export default function Welcome() {
     return(
         <div className="intro">
             <div className="introContent">
-                <span className="welcome">Hi there!<br /></span>
-                <span className="text"> My name is <span className="introName">Adriana Saavedra.</span> <br /></span>
-                <p className="paragraph">I'm a software engineer that takes joy in expanding & strengthening my skill-set in full-stack development. 
+                <span className="welcome">Hello, </span><br/>
+                <span className="text"> My name is <span>
+                <img src={name} className="introName" alt="Adriana Saavedra"/>
+                </span></span>
+                <p className="paragraph">I'm a software engineer that takes joy in expanding & strengthening<br />my skill-set in full-stack development. 
                 </p>
                 <img src={python} alt="python" className="icon"/>
                 <img src={javascript} alt="javascript" className="icon"/>
