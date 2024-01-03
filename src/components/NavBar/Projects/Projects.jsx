0a8501e368cb549ec01afd3739b1a84d92609954
adrimsaav <adrimsaav@gmail.com>
2024-01-03 02:20:44 -0500

    remove effects from projects section

--- src/components/NavBar/Projects/Projects.jsx
+++ src/components/NavBar/Projects/Projects.jsx
@@ -3,13 +3,6 @@ import project from "../../../assets/projects.png";
 import './Projects.css'
 
 export default function Projects() {
-    const [fadeIn, setFadeIn] = useState(false);
-
-    useEffect(() => {
-        setFadeIn(true);
-    }, []);
-
-
     return (
         <div className='projects-container'>
             <div className="content">
