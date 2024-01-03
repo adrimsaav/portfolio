642b7cc578964bf7895339df5cc741c1cf4717a9
adrimsaav <adrimsaav@gmail.com>
2024-01-03 02:24:33 -0500

    add contact header

--- src/components/NavBar/Contact/Contact.jsx
+++ src/components/NavBar/Contact/Contact.jsx
@@ -1,10 +1,14 @@
 import "./Contact.css";
+import contact from "../../../assets/contact.png"
 
 export default function Contact() {
     return (
-      <div>
-          Contact
-      </div>
+        <div className='contact-container'>
+            <div className="contact-content">
+                <span className="text">
+                    <img src={contact} className="contactHeader" alt="header" />
+                </span>
+            </div>
+        </div>
     );
-  }
-  
+}
