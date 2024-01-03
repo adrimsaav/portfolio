b36772217797939fa2d9e76b57a8c09b91989952
adrimsaav <adrimsaav@gmail.com>
2024-01-03 02:08:43 -0500

    add to projects section

--- src/components/NavBar/Projects/Projects.jsx
+++ src/components/NavBar/Projects/Projects.jsx
@@ -1,10 +1,22 @@
-import "./Projects.css";
+import React, { useState, useEffect } from 'react';
+import project from "../../../assets/projects.png";
+import './Projects.css'
 
 export default function Projects() {
+    const [fadeIn, setFadeIn] = useState(false);
+
+    useEffect(() => {
+        setFadeIn(true);
+    }, []);
+
+
     return (
-      <div>
-          Projects
-      </div>
+        <div className='projects-container'>
+            <div className="content">
+                <span className="text">
+                    <img src={project} className="projectHeader" alt="header" />
+                </span>
+            </div>
+        </div>
     );
-  }
-  
+}
