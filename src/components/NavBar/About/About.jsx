8a869a4eabe0f9ef056de21261fd4fcf1d94093d
adrimsaav <adrimsaav@gmail.com>
2024-01-09 12:48:03 -0500

    fix mobile display for Welcome card

--- src/components/NavBar/About/About.jsx
+++ src/components/NavBar/About/About.jsx
@@ -27,7 +27,7 @@ export default function About() {
                 <p className="paragraph">            
                     Hello there! I'm a software engineer based in Montgomery County, MD. With some prior coding experience, 
                     my genuine interest in software development blossomed in fall 2023. I'm continuously expanding my knowledge and skills, excited about the learning journey ahead. 
-                    Apart from coding, I have a love for music, movies, and the satisfaction of making breakthroughs in programming. Explore my portfolio and feel free to connect!
+                    Apart from coding, I have a love for music, movies, and making breakthroughs in programming. Explore my portfolio and feel free to connect!
                 </p>
                 </div>
             </div>
